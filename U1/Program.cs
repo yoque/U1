dc8b800119e28dc10473a8ea08323f319e170d3f
yoque <65117031+yoque@users.noreply.github.com>
2021-06-15 16:19:22 +0200

    Add project files.

---
+++ U1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace U1
+{
+    class Program
+    {
+        private static object employee;
+
+        static void Main(string[] args)
+        {
+            String[] employee = new String[];
+            String[] salary = new String[];
+
+            while (Console.ReadLine() != null)
+            {
+                int m;
+                Console.WriteLine("Ange namn på anställd: ");
+                employee[m] = Console.ReadLine();
+                Console.WriteLine("Ange lön för anställd: ");
+                salary[m] = Console.ReadLine();
+                m++;
+
+
+            }
+
+
+        }
+
+        private static getreg(x)
+        {
+
+            int i=x;
+                        while (i<=Program.employee[].length)
+            {
+                Console.WriteLine(Program.employee[i]);
+                i++;
+            }
+
+        }
+    }
+}
